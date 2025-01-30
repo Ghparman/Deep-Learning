c494daac51125231148d7fd18d3b0d879f48b606
GHParman <greysonparman@gmail.com>
2025-01-30 14:40:01 -0600

    test commit to check remote connection

--- linearRegression_concise.py
+++ linearRegression_concise.py
@@ -2,3 +2,36 @@ import numpy as np
 import torch
 from torch import nn
 from d2l import torch as d2l
+import matplotlib.pyplot as plt
+
+class LinearRegression(d2l.Module):  #@save
+    """The linear regression model implemented with high-level APIs."""
+    def __init__(self, lr):
+        super().__init__()
+        self.save_hyperparameters()
+        self.net = nn.LazyLinear(1)
+        self.net.weight.data.normal_(0, 0.01)
+        self.net.bias.data.fill_(0)
+    def forward(self, X):
+        return self.net(X)
+
+    def loss(self, y_hat, y):
+        fn = nn.MSELoss()
+        return fn(y_hat, y)
+
+    def configure_optimizers(self):
+        return torch.optim.SGD(self.parameters(), self.lr)
+
+    def get_w_b(self):
+        return (self.net.weight.data, self.net.bias.data)
+
+model = LinearRegression(lr=0.03)
+data = d2l.SyntheticRegressionData(w=torch.tensor([2, -3.4]), b=4.2)
+trainer = d2l.Trainer(max_epochs=3)
+trainer.fit(model, data)
+plt.show()
+
+w, b = model.get_w_b()
+
+print(f'error in estimating w: {data.w - w.reshape(data.w.shape)}')
+print(f'error in estimating b: {data.b - b}')
