ec5c61f9739167cee40c15e38e4f3f1a878f6438
GHParman <greysonparman@gmail.com>
2025-02-13 16:57:40 -0600

    added plotting

--- Project_1/LeNet_Parman.py
+++ Project_1/LeNet_Parman.py
@@ -11,24 +11,24 @@ import matplotlib.pyplot as plt
 
 #hyper parameters
 seed = 1
-num_epochs = 20
+num_epochs = 5
 num_classes = 10
 lr = 0.1
 loss_fn = torch.nn.CrossEntropyLoss()
-batch_size = 512
+batch_size = 32
 device = torch.device("cpu")
 
 class LeNet(nn.Module):
     def __init__(self,lr,num_classes):
         super().__init__()
         self.Network = nn.Sequential(
-            nn.LazyConv2d(6, kernel_size=5, padding=2), nn.Sigmoid(),
-            nn.AvgPool2d(kernel_size=2, stride=2),
-            nn.LazyConv2d(16, kernel_size=5), nn.Sigmoid(),
-            nn.AvgPool2d(kernel_size=2, stride=2),
+            nn.LazyConv2d(6, kernel_size=5, padding=2), nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.LazyConv2d(16, kernel_size=5), nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Flatten(),
-            nn.LazyLinear(120), nn.Sigmoid(),
-            nn.LazyLinear(84), nn.Sigmoid(),
+            nn.LazyLinear(120), nn.ReLU(),
+            nn.LazyLinear(84), nn.ReLU(),
             nn.LazyLinear(num_classes))
 
     def forward(self, inputs):
@@ -57,7 +57,8 @@ print("training...")
 
 model = LeNet(lr,num_classes)
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-
+train_loss_per_epoch = []
+val_loss_per_epoch = []
 for epoch in range(1, num_epochs + 1):
     train_loss = []
     val_loss = []
@@ -79,7 +80,20 @@ for epoch in range(1, num_epochs + 1):
         loss = loss_fn(model.forward(x), y)
 
         val_loss.append(loss.item())
-
+    train_loss_per_epoch.append(np.mean(train_loss))
+    val_loss_per_epoch.append(np.mean(val_loss))
     print("epoch: {}/{}, train loss: {:.3f} | val loss: {:.3f}".format(epoch, num_epochs, np.mean(train_loss),
                                                                        np.mean(val_loss)))
+fig, axs = plt.subplots(2)
+fig.suptitle(f"Training and Validation loss across {num_epochs} epochs")
+axs[0].plot(train_loss_per_epoch)
+axs[0].set_ylabel("training loss")
+axs[0].set_xticks([i for i in range(1, num_epochs+1)])
+axs[0].spines['bottom'].set_position(('data', 1))
+axs[1].plot(val_loss_per_epoch)
+axs[1].set_ylabel("validation loss")
+axs[1].set_xticks([i for i in range(1,num_epochs+1)])
+axs[1].spines['bottom'].set_position(('data', 1))
 
+
+plt.show()
